fc2a849ee8d9c5e5d4abc3e6e450a55d88e7059f
Anders Greve Sørensen <anders1709@gmail.com>
2023-10-31 08:55:39 +0100

    Setup maven project

---
+++ cdio3solo/src/test/java/cdio3solo/AppTest.java
@@ -0,0 +1,20 @@
+package cdio3solo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
